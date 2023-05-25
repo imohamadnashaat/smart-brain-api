2e4bec8abd9db0561b5b285bd88871fc0f44c671
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-05-25 21:45:45 +0300

    Add clarifai image endpoint

--- src/app.js
+++ src/app.js
@@ -90,4 +90,39 @@ app.put('/image', (req, res) => {
   }
 });
 
+app.post('/clarifaiImage', (req, res) => {
+  const { imageUrl } = req.body;
+  const PAT = process.env.PAT;
+  const USER_ID = process.env.USER_ID;
+  const APP_ID = process.env.APP_ID;
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  fetch('https://api.clarifai.com/v2/models/' + MODEL_ID + '/outputs', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => res.status(200).json(result))
+    .catch((error) => res.status(400).json(error));
+});
+
 export default app;
