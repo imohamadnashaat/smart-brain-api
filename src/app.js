ed41f3be1b53ce8def892af57953c2622e44a648
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-05-29 13:39:55 +0300

    Update database connection

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.PG_CONNECTION_STRING,
-    ssl: true,
+    ssl: process.env.SSL_STATUS === 'true',
   },
 });
 
