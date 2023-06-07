787c2240fd84fd3da19d7934db9731b2a2da6554
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-06-07 18:02:29 +0300

    Update database config

--- src/services/db.js
+++ src/services/db.js
@@ -1,14 +1,17 @@
+import 'dotenv/config';
 import knex from 'knex';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const db = knex({
+const dbConfig = {
   client: 'pg',
   connection: {
-    connectionString: process.env.PG_CONNECTION_STRING,
-    ssl: process.env.SSL_STATUS === 'true',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
   },
-});
+};
+
+const db = knex(dbConfig);
 
 export default db;
